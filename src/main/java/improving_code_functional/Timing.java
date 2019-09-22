1acb9cfa52f7548964c40ba597f2489081f344ed
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-09-22 16:08:32 +0200

    Isolates the output in printResult(). Defines a generic output via java.util.Consumer<String>. Defines a default Consumer just to avoid change all code in Example

--- src/main/java/improving_code_functional/Timing.java
+++ src/main/java/improving_code_functional/Timing.java
@@ -1,19 +1,29 @@
 package improving_code_functional;
 
 import java.util.Date;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 class Timing {
 
-  static <A> A timed(String description, Supplier<A> code) {
+  static <A> A timed(String description, Supplier<A> code) { // Just a default consumer
+    Consumer<String> defaultOutput = System.out::println;
+    return timed(description, defaultOutput, code);
+  }
+
+  static <A> A timed(String description, Consumer<String> output, Supplier<A> code) {
     final Date beforeCosts = new Date();
 
     A result = code.get();
 
     final long duration = new Date().getTime() - beforeCosts.getTime();
-    System.out.println(description + " took " + duration  + " milliseconds");
+    printResult(output, description, duration);
 
     return result;
+  }
+
+  private static void printResult(Consumer<String> output, String description, long duration) {
+    output.accept(description + " took " + duration  + " milliseconds");
   }
 
 }
