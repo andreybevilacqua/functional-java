1df362bf7f01f2dbbc03cc68b013a92076581b25
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2020-02-02 17:59:54 +0100

    Playing with final attributes, objects, and final concept

--- src/main/java/exercise/Test.java
+++ src/main/java/exercise/Test.java
@@ -7,16 +7,25 @@ public class Test {
 
   private final List foo; // the final here is to initialize the list only once.
 
-  public Test() {
+  private final int testIntValue; // in primitive variables, the final make it be set only once (memory and performance gain)
+
+  private final String testStringValue;
+
+  public Test(int a) {
     foo = new ArrayList ();
     foo.add("foo"); // Modification-1
     // foo = new ArrayList(); // As you can see here.
+
+    this.testIntValue = a;
+    this.testStringValue = "This is a first value for this object, but it will change";
+
+    // In the String case, we cannot re-initiate it;
+    //testStringValue = "Another value"; // This will generate compile error.
   }
   public static void main(String[] args) {
-    Test t = new Test();
+    Test t = new Test(1);
     t.foo.add("bar"); // Modification-2
     System.out.println("print - " + t.foo);
-
     // You still can change thinks inside of the object, but you cannot re-instantiate the object
     // or point it to another ArrayList, because it is a final object.
     // The final here is not about the content.
